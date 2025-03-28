48751b3180bff76ca9b87e228377bf8e0e1f1427
brestok <6234513@gmail.com>
2025-03-29 01:24:15 +0300

    Refactor App component and update homepage URL
    Simplify App component by removing unused `Routes` and directly rendering `Home`. Update `homepage` in `package.json` to correct the URL format.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import {Route, Routes} from 'react-router-dom';
 import Home from './Page/Home';
 import {useEffect} from "react";
 import Cookies from "js-cookie";
@@ -58,13 +57,7 @@ function App() {
     };
 
     useTokenRefresh();
-  return (
-
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
- 
-  );
+  return (<Home />);
 }
 
 export default App;
