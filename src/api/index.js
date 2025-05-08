01ab5175df802786a10f7984b5a36fa573f205e7
brestok <6234513@gmail.com>
2025-05-08 23:09:45 +0300

    fix url

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:8000',
+    baseURL: 'https://api.bundyonsol.xyz',
     headers: {
       'Accept': 'application/json',
       'Content-Type': 'application/json',
