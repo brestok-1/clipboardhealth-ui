3e873aa2f2839337b240cfd94bf75aa6c829067b
brestok <6234513@gmail.com>
2025-07-24 18:10:07 +0300

    Remove AgentPage, CallDetailPage, and related components along with API functions and utility files. This cleanup eliminates unused code and components, streamlining the codebase.

--- src/static/enums/PeriodType.js
+++
@@ -1,5 +0,0 @@
-export const PeriodType = Object.freeze({
-    Daily: 1,
-    Weekly: 2,
-    Monthly: 3
-});
--- src/static/enums/StatisticsType.js
+++
@@ -1,8 +0,0 @@
-export const StatisticsType = Object.freeze({
-    "Top Sales Reps by Total Call Duration": 1,
-    "Bottom Sales Reps by Total Call Duration": 2,
-    "Top Sales Reps by Total Calls Number": 3,
-    "Bottom Sales Reps by Total Calls Number": 4,
-    "Top Sales Reps by Cold Call Quality ": 5,
-    "Bottom Sales Reps by Cold Call Quality": 6
-});
